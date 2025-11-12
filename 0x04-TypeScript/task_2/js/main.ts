da7b007371292b57c7ab69aea313e85230af8c7b
Jefter83 <jefteradinkrah82@gmail.com>
2025-11-11 23:03:25 -0800

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,13 +1,48 @@
-// Director interface
+// Interfaces
 interface DirectorInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workDirectorTasks(): string;
 }
 
-// Teacher interface
 interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workTeacherTasks(): string;
 }
+
+// Classes
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Remote work enabled";
+  }
+
+  getCoffeeBreak(): string {
+    return "Coffee break granted";
+  }
+
+  workDirectorTasks(): string {
+    return "Executing director responsibilities";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Remote work not permitted";
+  }
+
+  getCoffeeBreak(): string {
+    return "Break not allowed";
+  }
+
+  workTeacherTasks(): string {
+    return "Teaching in progress";
+  }
+}
+
+// Factory function
+function createEmployee(salary: number | string): Director | Teacher {
+  const isLowSalary = typeof salary === "number" && salary < 500;
+  return isLowSalary ? new Teacher() : new Director();
+}
+

513b8efb819abde575378cc2d17f0348309c20f7
Jefter83 <jefteradinkrah82@gmail.com>
2025-11-11 22:53:11 -0800

    Create main.ts

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,13 @@
+// Director interface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// Teacher interface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}

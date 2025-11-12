a81f6c99f1f53f63691f53485e545881b6fbf6f4
Jefter83 <jefteradinkrah82@gmail.com>
2025-11-11 22:12:31 -0800

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -25,16 +25,6 @@ interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any;
-}
-
-// Define the Teacher interface
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
   [key: string]: any; // Allows additional properties
 }
 
@@ -43,12 +33,11 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-
 // Example usage
 const director1: Directors = {
   firstName: 'Jefter',
   lastName: 'Adinkrah',
-  location: 'Ghana',
+  location: 'London',
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
